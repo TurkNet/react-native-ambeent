02bb6e7fff2a1677300c97c28338de8c76d277cb
Refik ÖNAL <refikonal@gmail.com>
2019-09-12 15:03:16 +0300

    ADD wrapper method with default named arguments

--- index.ios.js
+++ index.ios.js
@@ -1,1 +1,5 @@
-export default {};
+export default {
+    sense() {
+        return null;
+    }
+};
--- index.js
+++ index.js
@@ -2,4 +2,23 @@ import { NativeModules } from 'react-native';
 
 const { Ambeent } = NativeModules;
 
-export default Ambeent;
+export default {
+    sense({
+        discoverNetwork = true,
+        measureSpeed = true,
+        detectRouterModel = true,
+    }) {
+        return Ambeent.sense(discoverNetwork, measureSpeed, detectRouterModel);
+    }
+};
+
+
+/*
+
+discoverNetwork = true; // This should be true, if you want to scan the n etwork.
+boolean measureSpeed = true; // This should be true, if you want to make measure ments.
+boolean detectRouterModel = true; // This should be true, if you want to detect R outer Model.
+double bw = sense(discoverNetwork, measureSpeed, detectRouterModel);
+Log.d(TAG, bw);
+
+*/
